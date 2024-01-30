4703d4188412dc0d41163887ce7c99f426d29041
marksikaundi <emmanuelmark117@gmail.com>
2024-01-30 13:41:24 +0200

    progress in course page

--- app/(router)/courses/page.js
+++ app/(router)/courses/page.js
@@ -1,9 +1,19 @@
 
 import React from 'react'
+import WelcomBanner from './_components/WelcomBanner'
 
 function Courses() {
   return (
-    <div>Courses</div>
+    <div className='grid grid-cols-1 md:grid-cols-3 p-5'>
+      {/* Left Container */}
+      <div className='col-span-2'>
+        <WelcomBanner/>
+      </div>
+      {/* Right Container */}
+      <div className=''>
+      Right Section
+      </div>
+    </div>
   )
 }
 

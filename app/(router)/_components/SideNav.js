f09fb039138286b3d207bc4675f6302f1920a8a3
marksikaundi <emmanuelmark117@gmail.com>
2024-01-30 12:26:47 +0200

    progress styling

--- app/(router)/_components/SideNav.js
+++ app/(router)/_components/SideNav.js
@@ -21,12 +21,20 @@ function SideNav() {
         }
     ]
   return (
-    <div>
+    <div className='p-5 bg-white shadow-sm border h-screen '>
         <Image src="/logo.svg"
         alt="logo"
         width={170}
         height={80}
         />
+        <div className=''>
+            {menu.map((item, index)=>(
+                <div className='flex gap-3 mt-1 p-3'>
+                    <item.icon size={24} />
+                    <h2>{item.name}</h2>
+                </div>
+            ))}
+        </div>
     </div>
   )
 }

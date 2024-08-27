913a0af2b3a4ea774841f98e84b1fb4009ef4f0a
marksikaundi <emmanuelmark117@gmail.com>
2024-08-27 16:01:10 +0200

    refactor: Update AuthScreen component for authentication and styling improvements

--- src/features/auth/components/auth-screen.tsx
+++ src/features/auth/components/auth-screen.tsx
@@ -1,12 +1,13 @@
-"use client"
+"use client";
 
+import { useState } from "react";
+import { SignInFlow } from "../types";
 
 export const AuthScreen = () => {
-    return (
-        <div>
-            Auth Screen
-        </div>
-    )
-}
-
-
+  const [state, setState] = useState<SignInFlow>("signIn");
+  return (
+    <div className="h-full flex items-center justify-center bg-[#5C3B58] ">
+      <div className="md:h-auto md:w-[420px]  ">Auth Screen</div>
+    </div>
+  );
+};

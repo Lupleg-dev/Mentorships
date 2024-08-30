a0af1ca7237ba61d633e7b60d4605db3e82dd8eb
marksikaundi <emmanuelmark117@gmail.com>
2024-08-30 07:52:53 +0200

    feat: Add Google as a provider for authentication
    This commit adds Google as a provider for authentication in the Convex application. The `convex/auth.ts` file has been modified to include the `Google` provider alongside the existing `GitHub` provider. Additionally, the `src/features/auth/components/sign-in-card.tsx` file has been updated to handle the sign-in process for both GitHub and Google providers.

--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -25,7 +25,7 @@ export const SignInCard = ({ setState }: SignInCardProps) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleProviderSignIn = (value: "github" | "google") => {
+  const onProviderSignIn = (value: "github" | "google") => {
     signIn(value);
   };
 
@@ -64,7 +64,7 @@ export const SignInCard = ({ setState }: SignInCardProps) => {
         <div className="flex flex-col gap-y-2.5">
           <Button
             type="button"
-            onClick={() => {}}
+            onClick={() => onProviderSignIn("google")}
             variant="outline"
             className="w-full relative"
             size="lg"
@@ -75,7 +75,7 @@ export const SignInCard = ({ setState }: SignInCardProps) => {
           </Button>
           <Button
             type="button"
-            onClick={() => handleProviderSignIn("github")}
+            onClick={() => onProviderSignIn("github")}
             variant="outline"
             className="w-full relative"
             size="lg"

a26a342abcdaf2dcf909609aaffb8537056a549e
marksikaundi <emmanuelmark117@gmail.com>
2024-08-30 10:21:03 +0200

    feat: Update UserButton component to include LogOut functionality
    The UserButton component in src/features/auth/components/user-button.tsx has been updated to include the LogOut icon from the lucide-react library. This allows users to sign out of their account. The unnecessary email variable has been removed from the component as well.
    
    Note: Please review and remove any unnecessary code or files before committing.

--- src/features/auth/components/user-button.tsx
+++ src/features/auth/components/user-button.tsx
@@ -9,7 +9,7 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { useCurrentUser } from "../hooks/use-current-user";
-import { Loader } from "lucide-react";
+import { Loader, LogOut } from "lucide-react";
 
 export const UserButton = () => {
   const { data, isLoading } = useCurrentUser();
@@ -22,7 +22,7 @@ export const UserButton = () => {
     return null;
   }
 
-  const { image, name, email } = data;
+  const { image, name } = data;
 
   const avatarFallback = name!.charAt(0).toUpperCase();
 
@@ -30,15 +30,16 @@ export const UserButton = () => {
     <DropdownMenu modal={false}>
       <DropdownMenuTrigger className="outline-none relative">
         <Avatar className="size-10 hover:opacity-75 transition">
-          <AvatarImage />
+          <AvatarImage alt={name} src={image} />
           <AvatarFallback>{avatarFallback}</AvatarFallback>
         </Avatar>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="center" side="right" className="w-60">
         <DropdownMenuLabel>Account</DropdownMenuLabel>
-        <DropdownMenuItem>Profile</DropdownMenuItem>
+        <DropdownMenuItem>{name}</DropdownMenuItem>
         <DropdownMenuItem>Settings</DropdownMenuItem>
         <DropdownMenuSeparator />
+        {/* <LogOut className="size-4 mr-2"/> */}
         <DropdownMenuItem>Sign out</DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>

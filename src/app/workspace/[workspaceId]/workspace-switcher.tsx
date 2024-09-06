91144106ad14e2d5c63fdd011247db28243935cd
marksikaundi <emmanuelmark117@gmail.com>
2024-09-06 17:05:56 +0200

    feat: Include Plus icon for creating a new workspace in workspace-switcher.tsx

--- src/app/workspace/[workspaceId]/workspace-switcher.tsx
+++ src/app/workspace/[workspaceId]/workspace-switcher.tsx
@@ -13,7 +13,7 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { useCreateWorkspaceModal } from "@/features/workspaces/store/use-create-workspace-modal";
-import { Loader } from "lucide-react";
+import { Loader, Plus } from "lucide-react";
 
 export const WorkspaceSwitcher = () => {
   const router = useRouter();
@@ -55,10 +55,23 @@ export const WorkspaceSwitcher = () => {
             className="cursor-pointer capitalize"
             key={workspace._id}
             onClick={() => router.push(`/workspace/${workspace._id}`)}
-          ></DropdownMenuItem>
+          >
+          <div className="size-9 relative overflow-hidden bg-[#F2F2F2] text-slate-800 font-semibold text-xl rounded-md flex items-center justify-center mr-2  ">
+
+              {workspace.name.charAt(0).toUpperCase()}
+            </div>
+            {workspace.name}
+
+          </DropdownMenuItem>
         ))}
-        <DropdownMenuItem>
-          <div className="size-9 relative overflow-hidden bg-[#F2F2F2] text-slate-800 font-semibold text-xl rounded-md flex items-center justify-center  "></div>
+        <DropdownMenuItem
+          className="cursor-pointer"
+          onClick={() => setOpen(true)}
+        >
+          <div className="size-9 relative overflow-hidden bg-[#F2F2F2] text-slate-800 font-semibold text-xl rounded-md flex items-center justify-center mr-2  ">
+            <Plus />
+          </div>
+          Create a new workspace
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>

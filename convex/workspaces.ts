49c5a8b4e6d1320e08e396c87e313c27ff186a00
marksikaundi <emmanuelmark117@gmail.com>
2024-10-08 13:19:42 +0200

    Refactor: Update PreferencesModel component in workspace header dropdown menu and add delete workspace functionality to PreferencesModel component

--- convex/workspaces.ts
+++ convex/workspaces.ts
@@ -5,10 +5,7 @@ import { auth } from "./auth";
 const generateCode = () => {
   const code = Array.from(
     { length: 6 },
-    () =>
-      "0123456789abcdefghijklmnopqrstuvwxyz"[
-        Math.floor(Math.random() * 36)
-      ]
+    () => "0123456789abcdefghijklmnopqrstuvwxyz"[Math.floor(Math.random() * 36)]
   ).join("");
   return code;
 };
@@ -93,5 +90,33 @@ export const getById = query({
     }
 
     return await ctx.db.get(args.id);
+  },
+});
+
+export const update = mutation({
+  args: {
+    id: v.id("workspaces"),
+    name: v.string(),
+  },
+  handler: async (ctx, args) => {
+    const userId = await auth.getUserId(ctx);
+
+    if (!userId) {
+      throw new Error("Unauthorized");
+    }
+
+    const member = await ctx.db
+      .query("members")
+      .withIndex("by_workspace_id_user_id", (q) =>
+        q.eq("workspaceId", args.id).eq("userId", userId)
+      )
+      .unique();
+
+    if (!member || member.role !== "admin") {
+      throw new Error("Unauthorized");
+    }
+    await ctx.db.patch(args.id, {
+      name: args.name,
+    });
   },
 });

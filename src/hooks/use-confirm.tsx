5b3995afa751325820ccf11ab7f7c69e2d194ab2
marksikaundi <emmanuelmark117@gmail.com>
2024-10-09 14:39:08 +0200

    Refactor: Add useConfirm hook for displaying confirmation dialogs

---
+++ src/hooks/use-confirm.tsx
@@ -0,0 +1,27 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+
+export const useConfirm = (title: string, message: string): [any, any] => {
+  const [promise, setPromise] = useState<{
+    resolve: (value: boolean) => void;
+  } | null>(null);
+
+  const confirm = () =>
+    new Promise((resolve, reject) => {
+      setPromise({ resolve });
+    });
+
+  const handleClose = () => {
+    setPromise(null);
+  };
+  return ["", ""];
+};

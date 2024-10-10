93612ca615b97d4a8fd904884365a2a6f546ef51
marksikaundi <emmanuelmark117@gmail.com>
2024-10-10 14:33:37 +0200

    Refactor: Fix missing return statement in ConfirmDialog component

--- src/hooks/use-confirm.tsx
+++ src/hooks/use-confirm.tsx
@@ -36,20 +36,22 @@ export const useConfirm = (title: string, message: string): [() => JSX.Element, 
   };
 
   const ConfirmDialog = () => {
-    <Dialog open={promise !== null}>
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle>{title}</DialogTitle>
-          <DialogDescription>{message}</DialogDescription>
-        </DialogHeader>
-        <DialogFooter className="pt-2">
-          <Button onClick={handleCancel} variant="outline">
-            Cancel
-          </Button>
-          <Button onClick={handleConfirm}>Confirm</Button>
-        </DialogFooter>
-      </DialogContent>
-    </Dialog>;
+    return (
+      <Dialog open={promise !== null}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>{title}</DialogTitle>
+            <DialogDescription>{message}</DialogDescription>
+          </DialogHeader>
+          <DialogFooter className="pt-2">
+            <Button onClick={handleCancel} variant="outline">
+              Cancel
+            </Button>
+            <Button onClick={handleConfirm}>Confirm</Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    );
   };
 
   return [ConfirmDialog, confirm]

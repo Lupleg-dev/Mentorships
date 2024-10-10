1419cfdca14b202ba669f20964624c475a95ca78
marksikaundi <emmanuelmark117@gmail.com>
2024-10-10 13:56:02 +0200

    Logics implemented

--- src/hooks/use-confirm.tsx
+++ src/hooks/use-confirm.tsx
@@ -2,6 +2,7 @@ import {
   Dialog,
   DialogContent,
   DialogDescription,
+  DialogFooter,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
@@ -25,9 +26,31 @@ export const useConfirm = (title: string, message: string): [any, any] => {
   };
 
   const handleCancel = () => {
-    promise?.resolve(false)
-    handleClose()
-    
-  }
-  return ["", ""];
+    promise?.resolve(false);
+    handleClose();
+  };
+
+  const handleConfirm = () => {
+    promise?.resolve(true);
+    handleClose();
+  };
+
+  const ConfirmDialog = () => {
+    <Dialog open={promise !== null}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>{title}</DialogTitle>
+          <DialogDescription>{message}</DialogDescription>
+        </DialogHeader>
+        <DialogFooter className="pt-2">
+          <Button onClick={handleCancel} variant="outline">
+            Cancel
+          </Button>
+          <Button onClick={handleConfirm}>Confirm</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>;
+  };
+
+  return [ConfirmDialog, confirm]
 };

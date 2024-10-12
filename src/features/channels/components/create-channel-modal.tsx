044a50c8bdb325c32d774cc4413333cd0014fad9
marksikaundi <emmanuelmark117@gmail.com>
2024-10-12 11:24:00 +0200

    Refactor: Add input field and create button to CreateChannelModal component

--- src/features/channels/components/create-channel-modal.tsx
+++ src/features/channels/components/create-channel-modal.tsx
@@ -8,9 +8,19 @@ import {
 } from "@/components/ui/dialog";
 import { useCreateChannelModal } from "../store/use-create-channel-modal";
 import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
 
 export const CreateChannelModal = () => {
   const [open, setOpen] = useCreateChannelModal();
+
+const [name, setName] = useState("")
+
+const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const  value  = e.target.value.replace(/\s+/g,"-").toLowerCase();
+    setName(value)
+}
+
   return (
     <Dialog open={open} onOpenChange={setOpen}>
       <DialogContent>
@@ -19,15 +29,22 @@ export const CreateChannelModal = () => {
         </DialogHeader>
         <form className="space-y-2">
             <Input
-            value=""
+            value={name}
             disabled={false}
-            onChange={() => {}}
+            onChange={handleChange}
             required
             autoFocus
             minLength={3}
             maxLength={80}
             placeholder="e.g. LuplegDevOps"
              />
+             <div className="flex justify-end">
+                <Button
+                disabled={false}
+                >
+                        Create
+                </Button>
+             </div>
 
         </form>
       </DialogContent>

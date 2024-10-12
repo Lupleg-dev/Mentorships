c76dae3791db66665a96d6c606f2ad4b42e1257a
marksikaundi <emmanuelmark117@gmail.com>
2024-10-12 11:03:17 +0200

    Refactor: Add CreateChannelModal component to Modals

---
+++ src/features/channels/components/create-channel-modal.tsx
@@ -0,0 +1,22 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { useCreateChannelModal } from "../store/use-create-channel-modal";
+
+export const CreateChannelModal = () => {
+  const [open, setOpen] = useCreateChannelModal();
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Create a new channel</DialogTitle>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
+};

7a3444d3339d9dbc38ef669ba7d90100a6377662
marksikaundi <emmanuelmark117@gmail.com>
2024-10-13 20:56:28 +0200

    Refactor: Update ChannelIdPage component to include chat input functionality

--- src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
@@ -1,46 +1,42 @@
-"use client"
+"use client";
 
 import { useGetChannel } from "@/features/channels/api/use-get-channel";
 import { useChannelId } from "@/hooks/use-channel-id";
 import { Loader, TriangleAlert } from "lucide-react";
 import { Header } from "./header";
+import { ChatInput } from "./chat-input";
 
-const ChannelIdPage= () => {
-    const channelId = useChannelId();
+const ChannelIdPage = () => {
+  const channelId = useChannelId();
 
-    const { data: channel, isLoading: channelLoading } = useGetChannel({
-        id: channelId,
-    });
+  const { data: channel, isLoading: channelLoading } = useGetChannel({
+    id: channelId,
+  });
 
-    if (channelLoading) {
-        return (
-            <div className="flex flex-1  h-full items-center justify-center ">
-                <Loader className="size-5 animate-spin text-muted-foreground " />
-            </div>
-        );
-    }
-
-    if (!channel) {
-        return (
-            <div className="flex flex-1 flex-col gap-y-2 h-full items-center justify-center ">
-                <TriangleAlert className="size-5 text-muted-foreground " />
-                <span className="text-sm text-muted-foreground">
-                    Channel not found
-                </span>
-            </div>
-        );
-    }
-
-
-
+  if (channelLoading) {
     return (
-        <div className="flex flex-col h-full">
-            <Header title={channel.name} />
+      <div className="flex flex-1  h-full items-center justify-center ">
+        <Loader className="size-5 animate-spin text-muted-foreground " />
+      </div>
+    );
+  }
 
-        </div>
-    )
-}
+  if (!channel) {
+    return (
+      <div className="flex flex-1 flex-col gap-y-2 h-full items-center justify-center ">
+        <TriangleAlert className="size-5 text-muted-foreground " />
+        <span className="text-sm text-muted-foreground">Channel not found</span>
+      </div>
+    );
+  }
 
-export default ChannelIdPage
+  return (
+    <div className="flex flex-col h-full">
+      <Header title={channel.name} />
+      <div className="flex-1" />
+      <ChatInput />
+    </div>
+  );
+};
 
-
+export default ChannelIdPage;

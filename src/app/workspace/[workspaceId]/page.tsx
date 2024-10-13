73e503361bc794931c676a62695977a9e753474a
marksikaundi <emmanuelmark117@gmail.com>
2024-10-13 13:12:58 +0200

    Refactor: Update WorkspaceIdPage component to fetch workspace information and channels
    This commit refactors the WorkspaceIdPage component to fetch the workspace information and channels using the useGetWorkspace and useGetChannels hooks. It also adds the useCreateChannelModal hook for creating channels. The component now redirects to the workspace page if the user is already a member. This improves the functionality and data fetching in the join page.

--- src/app/workspace/[workspaceId]/page.tsx
+++ src/app/workspace/[workspaceId]/page.tsx
@@ -1,7 +1,18 @@
 "use client";
 
+import { useGetChannels } from "@/features/channels/api/use-get-channels";
+import { useCreateChannelModal } from "@/features/channels/store/use-create-channel-modal";
+import { useGetWorkspace } from "@/features/workspaces/api/use-get-workspace";
+import { useWorkspaceId } from "@/hooks/use-workspace-id";
+import { useRouter } from "next/navigation";
 
 const WorkspaceIdPage = () => {
+  const workspaceId = useWorkspaceId();
+  const [open, setOpen] = useCreateChannelModal();
+  const router = useRouter();
+
+  const { data: workspace, isLoading: workspaceLoading } = useGetWorkspace({ id: workspaceId });
+  const { data: channels, isLoading: channelsLoading } = useGetChannels({ workspaceId }); 
 
   return <div>Workspace id Page </div>;
 };

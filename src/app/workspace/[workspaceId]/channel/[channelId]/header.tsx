ea53724f248df5fb28d06e36b80097b379a50659
marksikaundi <emmanuelmark117@gmail.com>
2024-10-13 14:54:10 +0200

    Refactor: Update Header component to include channel options dialog

--- src/app/workspace/[workspaceId]/channel/[channelId]/header.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/header.tsx
@@ -1,7 +1,54 @@
+import { Button } from "@/components/ui/button";
+import { FaChevronDown } from "react-icons/fa";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { TrashIcon } from "lucide-react";
+
 interface HeaderProps {
   title: string;
 }
 
 export const Header = ({ title }: HeaderProps) => {
-  return <div>Header {title}</div>;
+  return (
+    <div className="bg-white border-b h-[49px] flex items-center px-4 overflow-hidden ">
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button
+            variant="ghost"
+            size="sm"
+            className="text-lg font-semibold px-2 overflow-hidden w-auto"
+          >
+            <span className="truncate"># {title}</span>
+            <FaChevronDown className="size-2.5 ml-2 " />
+          </Button>
+        </DialogTrigger>
+        <DialogContent className="p-0 bg-gray-50 overflow-hidden">
+          <DialogHeader className="p-4 border-b bg-white">
+            <DialogTitle># {title}</DialogTitle>
+          </DialogHeader>
+          <div className="px-4 pb-4 flex flex-col gap-y-2">
+            <div className="px-5 py-4 bg-white rounded-lg border cursor-pointer hover:bg-gray-50">
+              <div className="flex items-center justify-between">
+                <p className="text-sm font-semibold">Channel name</p>
+                <p className="text-sm text-[#1264a3] hover:underline font-semibold ">
+                  Edit
+                </p>
+              </div>
+              <p className="text-sm "># {title}</p>
+            </div>
+            <button className="flex items-center gap-x-2 px-5 py-4 bg-white rounded-lg cursor-pointer border hover:bg-gray-50  text-rose-600 ">
+              <TrashIcon className="size-4" />
+              <p className="text-sm font-semibold">Delete channel</p>
+            </button>
+          </div>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
 };

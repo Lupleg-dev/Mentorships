74823e45b0707170e50676571cd1904caea01588
marksikaundi <emmanuelmark117@gmail.com>
2024-10-13 13:27:35 +0200

    Refactor: Update WorkspaceIdPage component to fetch workspace information and channels

--- src/app/workspace/[workspaceId]/page.tsx
+++ src/app/workspace/[workspaceId]/page.tsx
@@ -4,17 +4,63 @@ import { useGetChannels } from "@/features/channels/api/use-get-channels";
 import { useCreateChannelModal } from "@/features/channels/store/use-create-channel-modal";
 import { useGetWorkspace } from "@/features/workspaces/api/use-get-workspace";
 import { useWorkspaceId } from "@/hooks/use-workspace-id";
+import { Loader, TriangleAlert } from "lucide-react";
 import { useRouter } from "next/navigation";
+import { useEffect, useMemo } from "react";
 
 const WorkspaceIdPage = () => {
   const workspaceId = useWorkspaceId();
   const [open, setOpen] = useCreateChannelModal();
   const router = useRouter();
 
-  const { data: workspace, isLoading: workspaceLoading } = useGetWorkspace({ id: workspaceId });
-  const { data: channels, isLoading: channelsLoading } = useGetChannels({ workspaceId }); 
+  const { data: workspace, isLoading: workspaceLoading } = useGetWorkspace({
+    id: workspaceId,
+  });
+  const { data: channels, isLoading: channelsLoading } = useGetChannels({
+    workspaceId,
+  });
 
-  return <div>Workspace id Page </div>;
+  const channelId = useMemo(() => channels?.[0]?._id, [channels]);
+
+  useEffect(() => {
+    if (workspaceLoading || channelsLoading || !workspace) return;
+
+    if (channelId) {
+      router.push(`/workspace/${workspaceId}/channel/${channelId}`);
+    } else if (!open) {
+      setOpen(true);
+    }
+  }, [
+    channelId,
+    channelsLoading,
+    workspace,
+    workspaceLoading,
+    open,
+    setOpen,
+    router,
+    workspaceId,
+  ]);
+
+  if (workspaceLoading || channelsLoading) {
+    return (
+      <div className="h-full flex-1 flex items-center justify-center flex-col gap-y-2">
+        <Loader className="size-6 text-muted-foreground animate-spin" />
+      </div>
+    );
+  }
+
+  if (!workspace) {
+    return (
+      <div className="h-full flex-1 flex items-center justify-center flex-col gap-y-2">
+        <TriangleAlert className="size-6 text-muted-foreground animate-spin" />
+        <span className="text-sm text-muted-foreground">
+          Workspace not found. Please check the URL and try again.
+        </span>
+      </div>
+    );
+  }
+
+  return null;
 };
 
 export default WorkspaceIdPage;

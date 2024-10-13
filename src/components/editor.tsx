dbb873337c1e0773716d1e6197956e966d8be276
marksikaundi <emmanuelmark117@gmail.com>
2024-10-13 21:05:45 +0200

    Update package dependencies

--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -1,11 +1,13 @@
-
-
 const Editor = () => {
-    return (
+  return (
+    <div className="flex flex-col">
+      <div className="flex flex-col border-slate-200 rounded-md overflow-hidden focus-within:border-slate-300 focus-within:shadow-sm transition bg-white">
         <div>
-            Editor
+            Efitor
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Editor
+export default Editor;

f1e023d2f29cad9a8a965d5a441d02c8233c1217
marksikaundi <emmanuelmark117@gmail.com>
2024-10-14 22:18:17 +0200

    Refactor: Update dynamic import for Quill text editor in ChatInput component

--- src/app/workspace/[workspaceId]/channel/[channelId]/chat-input.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/chat-input.tsx
@@ -1,7 +1,6 @@
 import dynamic from "next/dynamic"
 
 const Editor = dynamic(() => import("@/components/editor"), { ssr: false })
-
 export const ChatInput = () => {
     return (
         <div className="px-5 w-full">

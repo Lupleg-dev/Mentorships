d87ddfc28bcb6e16c92cf84dee9912f5cfb797da
marksikaundi <emmanuelmark117@gmail.com>
2024-10-15 22:35:32 +0200

    Refactor: Update Editor component to include dynamic import for Quill text editor and additional UI buttons

--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -3,19 +3,26 @@ import { PiTextAa } from "react-icons/pi";
 import { MdSend } from "react-icons/md";
 
 import "quill/dist/quill.snow.css";
-import { MutableRefObject, useEffect, useLayoutEffect, useRef } from "react";
+import {
+  MutableRefObject,
+  useEffect,
+  useLayoutEffect,
+  useRef,
+  useState,
+} from "react";
 import { ImageIcon, Smile } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Hint } from "./hint";
 import { Delta, Op } from "quill/core";
+import { cn } from "@/lib/utils";
 
 type EditorValue = {
   image: File | null;
   body: string;
-}
+};
 
 interface EditorProps {
-  onSubmit: ({ image, body}: EditorValue) => void;
+  onSubmit: ({ image, body }: EditorValue) => void;
   onCancel?: () => void;
   placeholder?: string;
   defaultValue?: Delta | Op[];
@@ -24,29 +31,30 @@ interface EditorProps {
   variant?: "create" | "update";
 }
 
-const Editor = ({ 
+const Editor = ({
   onSubmit,
   onCancel,
   placeholder = "Write something...",
   defaultValue = [],
   disabled = false,
   innerRef,
-  variant = "create" }: EditorProps) => {
+  variant = "create",
+}: EditorProps) => {
+  const [text, setText] = useState("");
 
   const containerRef = useRef<HTMLDivElement>(null);
-  const submitRef = useRef(onSubmit)
-  const placeholderRef = useRef(placeholder)
-  const quillRef = useRef<Quill | null>(null)
-  const defaultValueRef = useRef(defaultValue)
-  const disabledRef = useRef(disabled)
+  const submitRef = useRef(onSubmit);
+  const placeholderRef = useRef(placeholder);
+  const quillRef = useRef<Quill | null>(null);
+  const defaultValueRef = useRef(defaultValue);
+  const disabledRef = useRef(disabled);
 
   useLayoutEffect(() => {
     submitRef.current = onSubmit;
     placeholderRef.current = placeholder;
     defaultValueRef.current = defaultValue;
-    disabledRef.current = disabled
-  })
-
+    disabledRef.current = disabled;
+  });
 
   useEffect(() => {
     if (!containerRef.current) return;
@@ -61,14 +69,37 @@ const Editor = ({
       placeholder: placeholderRef.current,
     };
 
-    new Quill(editorContainer, options);
+    const quill = new Quill(editorContainer, options);
+    quillRef.current = quill;
+    quillRef.current.focus();
+
+    if (innerRef) {
+      innerRef.current = quill;
+    }
+
+    quill.setContents(defaultValueRef.current);
+    setText(quill.getText());
+
+    quill.on(Quill.events.TEXT_CHANGE, () => {
+      setText(quill.getText());
+    });
 
     return () => {
+      quill.off(Quill.events.TEXT_CHANGE);
       if (container) {
         container.innerHTML = "";
       }
+      if (quillRef.current) {
+        quillRef.current = null;
+      }
+      if (innerRef) {
+        innerRef.current = null;
+      }
     };
-  }, []);
+  }, [innerRef]);
+
+  const isEmpty = text.replace(/<(.|\n)*?>/g, "").trim().length === 0;
+  console.log({ isEmpty, text });
 
   return (
     <div className="flex flex-col">
@@ -76,23 +107,38 @@ const Editor = ({
         <div ref={containerRef} className="h-full ql-custom " />
         <div className="flex px-2 pb-2 z-[5]">
           <Hint label="Hide formatting">
-          <Button size="iconSm" variant="ghost" disabled={false} onClick={() => {}}>
-            <PiTextAa className="size-4" />
-          </Button>
+            <Button
+              size="iconSm"
+              variant="ghost"
+              disabled={false}
+              onClick={() => {}}
+            >
+              <PiTextAa className="size-4" />
+            </Button>
           </Hint>
 
-          <Hint label="Emoji" >
-          <Button size="iconSm" variant="ghost" disabled={false} onClick={() => {}}>
-            <Smile className="size-4" />
-          </Button>
+          <Hint label="Emoji">
+            <Button
+              size="iconSm"
+              variant="ghost"
+              disabled={false}
+              onClick={() => {}}
+            >
+              <Smile className="size-4" />
+            </Button>
           </Hint>
 
           {variant === "create" && (
-          <Hint label="Image">
-          <Button size="iconSm" variant="ghost" disabled={false} onClick={() => {}}>
-            <ImageIcon className="size-4" />
-          </Button>
-          </Hint>
+            <Hint label="Image">
+              <Button
+                size="iconSm"
+                variant="ghost"
+                disabled={false}
+                onClick={() => {}}
+              >
+                <ImageIcon className="size-4" />
+              </Button>
+            </Hint>
           )}
 
           {variant === "update" && (
@@ -102,23 +148,35 @@ const Editor = ({
                 size="sm"
                 onClick={() => {}}
                 disabled={false}
-              >Cancel</Button>
+              >
+                Cancel
+              </Button>
               <Button
-              disabled={false}
+                disabled={false}
                 onClick={() => {}}
                 size="sm"
                 className=" bg-[#007a5a] hover:bg-[#007a5a]/80 text-white  "
-              >Send</Button>
-
+              >
+                Send
+              </Button>
             </div>
           )}
 
           {variant === "create" && (
-          <Button disabled={false} onClick={() => {}} size="iconSm" className="ml-auto bg-[#007a5a] hover:bg-[#007a5a]/80 text-white  "  >
-            <MdSend className="size-4" />
-          </Button>
+            <Button
+              disabled={disabled || isEmpty}
+              onClick={() => {}}
+              size="iconSm"
+              className={cn(
+                "ml-auto",
+                isEmpty
+                  ? " bg-white hover:bg-white/80 text-muted-foreground"
+                  : " bg-[#007a5a] hover:bg-[#007a5a]/80 text-white"
+              )}
+            >
+              <MdSend className="size-4" />
+            </Button>
           )}
-
         </div>
       </div>
 

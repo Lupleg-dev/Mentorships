d7422fad3585940f4605853ec4e95e84a1576e46
marksikaundi <emmanuelmark117@gmail.com>
2024-10-19 23:49:25 +0200

    Refactor: Update EmojiPopover component to remove unused code and import emoji data

--- src/components/emoji-popover.tsx
+++ src/components/emoji-popover.tsx
@@ -10,42 +10,4 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
-import { useState } from "react";
-import data from "@emoji-mart/data"; // Import emoji data
 
-interface EmojiPopoverProps {
-  children: React.ReactNode;
-  hint?: string;
-  onEmojiSelect: (emoji: any) => void;
-}
-
-export const EmojiPopover = ({
-  children,
-  hint = "Emoji",
-  onEmojiSelect,
-}: EmojiPopoverProps) => {
-  const [popoverOpen, setPopoverOpen] = useState(false);
-  const [tooltipOpen, setTooltipOpen] = useState(false);
-
-  return (
-    <TooltipProvider>
-      <Popover open={popoverOpen} onOpenChange={setPopoverOpen}>
-        <Tooltip
-          open={tooltipOpen}
-          onOpenChange={setTooltipOpen}
-          delayDuration={50}
-        >
-          <PopoverTrigger asChild>
-            <TooltipTrigger asChild>{children}</TooltipTrigger>
-          </PopoverTrigger>
-          <TooltipContent className="bg-black text-white border border-white/5">
-            <p className="font-medium text-xs">{hint}</p>
-          </TooltipContent>
-        </Tooltip>
-        <PopoverContent className="p-0 w-full border-none shadow-none">
-          {/* <Picker data /> TODO: Implement emoji picker */}
-        </PopoverContent>
-      </Popover>
-    </TooltipProvider>
-  );
-};
